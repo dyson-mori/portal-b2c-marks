c8d553e436bdb8a11d7254d499593ec33c299201
dyson <supp.programming@gmail.com>
2024-07-22 05:48:07 -0300

    [damn]

--- src/app/panel/actions.ts
+++ src/app/panel/actions.ts
@@ -9,6 +9,7 @@ export async function getProductById(id: string): Promise<ProductsProps | null> 
 
   const res = await fetch(`${process.env.NEXT_URL}/api/product?id=${id}`, {
     method: 'GET',
+    cache: 'no-store',
     next: {
       tags: ['panel-product']
     },
@@ -18,7 +19,7 @@ export async function getProductById(id: string): Promise<ProductsProps | null> 
   });
 
   if (!res.ok) {
-    throw new Error('Product by Id');
+    throw new Error('Get Product by Id Failed');
   };
 
   return await res.json();
@@ -37,7 +38,7 @@ export async function getProducts(): Promise<ProductsProps[]> {
   });
 
   if (!res.ok) {
-    throw new Error('Product Panel')
+    throw new Error('Get Products Panel Failed')
   };
 
   return await res.json();
@@ -46,6 +47,7 @@ export async function getProducts(): Promise<ProductsProps[]> {
 export async function getCategories(): Promise<CategoryProps[]> {
   const res = await fetch(`${process.env.NEXT_URL}/api/category`, {
     method: 'GET',
+    cache: 'no-store',
     next: {
       tags: ['categories']
     },
@@ -55,7 +57,7 @@ export async function getCategories(): Promise<CategoryProps[]> {
   });
 
   if (!res.ok) {
-    throw new Error('Product by Id')
+    throw new Error('Category Error 502')
   };
 
   return await res.json();
